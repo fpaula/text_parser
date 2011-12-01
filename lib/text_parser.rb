3fc94bc5a049046b2a96149eeeaf2d30a10fd328
Fred <frederico.paula@abril.com.br>
2011-12-01 10:43:35 -0200

    Dictionary as an optional argument

--- lib/text_parser.rb
+++ lib/text_parser.rb
@@ -1,11 +1,19 @@
 module TextParser
-  def parse(dictionary)
+  def parse(dictionary = nil)
     result = []
-    regex = Regexp.new(dictionary.join("|"))
-    match_result = self.scan(regex)
+    text = process_text
+    dictionary = text.split(" ") unless dictionary
+    regex = Regexp.new(dictionary.join("|"), Regexp::IGNORECASE)
+    match_result = text.scan(regex).map{|i| i.downcase}
     match_result.each do |w|
       result << {:hits => match_result.count(w), :word => w} unless result.select{|r| r[:word] == w}.shift
     end 
     result
+  end
+  
+  private
+  
+  def process_text
+    self.gsub(/[^\w\s\-]/, "")
   end
 end
--- test/text_parser_test.rb
+++ test/text_parser_test.rb
@@ -11,4 +11,19 @@ class TextParserTest < Test::Unit::TestCase
     text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pretium consectetur."
     assert_equal text.parse(["dolor", "consectetur"]), [{:word => "dolor", :hits => 1}, {:word => "consectetur", :hits => 2}]
   end
+  
+  def test_should_parse_without_dictionary
+    text = "test test"
+    assert_equal text.parse, [{:word => "test", :hits => 2}]
+  end
+  
+  def test_should_remove_some_characters
+    text = "Test? Test. Yes, test!"
+    assert_equal text.parse, [{:word => "test", :hits => 3}, {:word => "yes", :hits => 1}]
+  end
+  
+  def test_should_not_parse
+    text = "test"
+    assert_equal text.parse(['abc']), []
+  end
 end
