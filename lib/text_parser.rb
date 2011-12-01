06c48a95fe413634abb31485aeb0fe6e48cbf344
Fred <frederico.paula@abril.com.br>
2011-12-01 15:14:26 -0200

    More sort options

--- lib/text_parser.rb
+++ lib/text_parser.rb
@@ -1,14 +1,22 @@
 module TextParser
-  def parse(dictionary = nil)
+  def parse(args = {})
+    options = {
+      :dictionary => nil,
+      :order => :word,
+      :order_direction => :asc,
+      :negative_dictionary => nil
+    }.merge(args)
     result = []
     text = process_text
-    dictionary = text.split(" ") unless dictionary
-    regex = Regexp.new(dictionary.join("|"), Regexp::IGNORECASE)
+    options[:dictionary] = text.split(" ") unless options[:dictionary]
+    regex = Regexp.new(options[:dictionary].join("|"), Regexp::IGNORECASE)
     match_result = text.scan(regex).map{|i| i.downcase}
     match_result.each do |w|
       result << {:hits => match_result.count(w), :word => w} unless result.select{|r| r[:word] == w}.shift
     end 
-    result.sort_by{|i| i[:word]}
+    result = result.sort_by{|i| i[options[:order]]}
+    result.reverse! if options[:order_direction] == :desc
+    result
   end
   
   private
--- test/text_parser_test.rb
+++ test/text_parser_test.rb
@@ -3,13 +3,14 @@ require "string"
 require "text_parser"
 
 class TextParserTest < Test::Unit::TestCase
+
   def test_should_have_method_parse
     assert "some text".methods.select{|a| a=~/parse/}.count > 0
   end
   
   def test_should_parse
     text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pretium consectetur."
-    assert_equal text.parse(["dolor", "consectetur"]), [{:word => "consectetur", :hits => 2}, {:word => "dolor", :hits => 1}]
+    assert_equal text.parse(:dictionary => ["dolor", "consectetur"]), [{:word => "consectetur", :hits => 2}, {:word => "dolor", :hits => 1}]
   end
   
   def test_should_parse_without_dictionary
@@ -24,14 +25,37 @@ class TextParserTest < Test::Unit::TestCase
   
   def test_should_return_an_empty_array
     text = "test"
-    assert_equal text.parse(['abc']), []
+    assert_equal text.parse(:dictionary => ['abc']), []
+  end
+
+  def test_should_order_by_word_asc
+    text = " beta omega gamma alpha gamma"
+    result = [{:word => "alpha",  :hits => 1}, 
+              {:word => "beta",   :hits => 1},
+              {:word => "gamma",  :hits => 2},
+              {:word => "omega",  :hits => 1}]
+    assert_equal text.parse, result
+    assert_equal text.parse(:order => :word), result
+    assert_equal text.parse(:order => :word, :order_direction => :asc), result
   end
   
-  def test_should_order
-    text = " beta omega gamma alpha gamma"
-    assert_equal text.parse, [{:word => "alpha",  :hits => 1}, 
-                              {:word => "beta",   :hits => 1},
-                              {:word => "gamma",  :hits => 2},
-                              {:word => "omega",  :hits => 1}]
+  def test_should_order_by_word_desc
+    assert_equal "aaa zzz".parse(:order => :word, :order_direction => :desc), [{:word => "zzz",  :hits => 1}, {:word => "aaa",  :hits => 1}]
+  end
+ 
+  def test_should_order_by_hits_asc
+    text = "gamma alpha gamma beta alpha gamma"
+    result = [{:word => "beta",  :hits => 1},
+              {:word => "alpha", :hits => 2},
+              {:word => "gamma", :hits => 3}]
+    assert_equal text.parse(:order => :hits), result
+    assert_equal text.parse(:order => :hits, :order_direction => :asc), result
+  end
+  
+  def test_should_order_by_hits_desc
+    text = "gamma alpha gamma beta alpha gamma"
+    assert_equal text.parse(:order => :hits, :order_direction => :desc), [{:word => "gamma",  :hits => 3},
+                                                                          {:word => "alpha",  :hits => 2},
+                                                                          {:word => "beta",   :hits => 1}]
   end
 end
